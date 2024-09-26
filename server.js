f970a1ca8b69f3a11a9a4e59567602a246cf6ce5
Reuben <grerlich@gmail.com>
2024-09-26 14:58:25 -0400

    fruits to mongoose

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 const Fruit = require("./models/fruit.js");
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
@@ -16,6 +17,15 @@ app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
 });
 
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect("/fruits/new");
+});
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
